8b53f8bade5931d2de35e0a51c5fca43c505efdd
wikidrugss <an.camposp@alumnos.duoc.cl>
2019-12-01 03:19:18 -0300

    modificacion css

--- wikidrugs/urls.py
+++ wikidrugs/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url, include
+from rest_framework import routers
+from drugs.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('drugs.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
